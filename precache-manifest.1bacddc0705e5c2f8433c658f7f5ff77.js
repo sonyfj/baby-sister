5c547b080eaf22a7e2d25edd89b956daf5f35690
Sony Jose <sonyfj@Sonys-Mac-mini.local>
2020-02-07 22:55:03 -0800

    Updates

---
+++ precache-manifest.1bacddc0705e5c2f8433c658f7f5ff77.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02fccb8a5a38cfae18279609ab1e7034",
+    "url": "/baby-sister/index.html"
+  },
+  {
+    "revision": "dfb0c7e11d8d663287cf",
+    "url": "/baby-sister/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bb18c4173840650641d2",
+    "url": "/baby-sister/static/js/2.640d2b16.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/baby-sister/static/js/2.640d2b16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb0c7e11d8d663287cf",
+    "url": "/baby-sister/static/js/main.d7275b5e.chunk.js"
+  },
+  {
+    "revision": "2d85d7b8bde46246d958",
+    "url": "/baby-sister/static/js/runtime-main.507177e9.js"
+  },
+  {
+    "revision": "c3bf8f879c331f2c5c92cb4391f925fc",
+    "url": "/baby-sister/static/media/boy.c3bf8f87.png"
+  },
+  {
+    "revision": "953fae91a68bb07e4a0340e12ea5fef5",
+    "url": "/baby-sister/static/media/girl.953fae91.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/baby-sister/static/media/logo.5d5d9eef.svg"
+  }
+]);
