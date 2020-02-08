6c407ccac090cbba00b027fd3b36124ad0c4b846
Sony Jose <sonyfj@Sonys-Mac-mini.local>
2020-02-07 22:52:34 -0800

    confetti, izzy, babies

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import useWindowSize from 'react-use/lib/useWindowSize'
-import Confetti from 'react-confetti'
+import Confetti from 'react-confetti';
+import boy from './images/boy.png';
+import girl from './images/girl.png';
 
 function App() {
 
@@ -11,9 +13,27 @@ function App() {
   return (
     <div className="App">
       <Confetti
-      width={width}
-      height={height}
-    />
+        width={width}
+        height={height}
+      />
+      <div width="100%" style={{paddingTop: "60px"}}>
+        <div style={{margin: "0 auto", width: "700px"}}>
+        <img src="http://c2.staticflickr.com/2/1596/23948188374_77061251e2_z.jpg"
+            style={{marginLeft: '-70px'}}>
+        </img>
+        <div style={{marginTop: "-200px"}}>
+        <div style={{display: "flex", width: "700px",
+              justifyContent: "space-between"}} >
+              <div>
+                <img width="250px" src={girl} />
+              </div>
+              <div>
+                <img width="250px" src={boy} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
